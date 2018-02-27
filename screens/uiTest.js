dee2a3617ec7b2d196bda7a6602ef34602f80f80
SkyMX99 - <alex.lewis@gmail.com>
2018-02-27 10:55:33 -0600

    adds UI Kitten and React Navigation

---
+++ screens/uiTest.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import {
+    Platform,
+    StyleSheet,
+    Text,
+    View,
+    Image,
+    ScrollView
+} from 'react-native';
+import { RkButton, RkCard, RkTheme, RkText } from 'react-native-ui-kitten';
+
+
+export default class UITest extends Component {
+
+
+
+    render() {
+        return (
+            <ScrollView>
+                <RkCard rkType='story'>
+                    <Image rkCardImg source={require('../images/skyline.jpg')} />
+                    <View rkCardHeader>
+                        <RkText rkType='header' style={{ fontSize: 30 }}>Once upon a time</RkText>
+                    </View>
+                    <View rkCardContent>
+                        <RkText style={{ textAlign: 'center' }}>
+                            One morning, when Gregor Samsa woke from happy dreams,
+                            he found himself transformed in ...
+                    </RkText>
+                    </View>
+                    <View rkCardFooter>
+                        <RkButton rkType='small outline'>Learn More</RkButton>
+                        <RkButton rkType='small'>Read later</RkButton>
+                    </View>
+                </RkCard>
+            </ScrollView>
+        )
+    }
+}
+
+RkTheme.setType('RkCard', 'story', {
+    img: {
+        height: 250,
+        opacity: 0.7
+    },
+    header: {
+        alignSelf: 'center',
+
+    },
+    content: {
+        alignSelf: 'center'
+    }
+});
