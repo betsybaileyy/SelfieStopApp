0f5fc3b46687497999c18462d2c2ed06c49a07b5
SkyMX99 - <alex.lewis@gmail.com>
2018-03-02 14:24:07 -0600

    fixing android project

--- android/app/src/main/java/com/selfiestop/MainApplication.java
+++ android/app/src/main/java/com/selfiestop/MainApplication.java
@@ -3,10 +3,6 @@ package com.selfiestop;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.wix.RNCameraKit.RNCameraKitPackage;
-import com.wix.RNCameraKit.RNCameraKitPackage;
-import com.wix.RNCameraKit.RNCameraKitPackage;
-import com.wix.RNCameraKit.RNCameraKitPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -28,11 +24,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNCameraKitPackage(),
-            new RNCameraKitPackage(),
-            new RNCameraKitPackage(),
-            new RNCameraKitPackage(),
-          new RNCameraKitPackage()
+            new RNCameraKitPackage()
       );
     }
 
