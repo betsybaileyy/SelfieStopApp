958e5c1131b5eb4ef005161936b8e884ca27bb47
Monty Khanounsay <montykhanovnsay@gmail.com>
2018-03-07 10:55:33 -0600

    Profile pages updated

--- components/ProfilePhotos.js
+++ components/ProfilePhotos.js
@@ -1,159 +1,48 @@
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View, Image, ScrollView } from 'react-native';
 import { RkButton, RkCard, RkTheme, RkText, RkModalImg, } from 'react-native-ui-kitten';
-
+import * as UserService from './services/userProfile';
 export default class ProfilePhotos extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selfies: [],
+            user: ''
+        };
+    }
+
+    async componentDidMount() {
+        console.log('did mount');
+        this.all();
+    }
+
+    async all() {
+        try {
+            const user = await UserService.all(1);
+            console.log(user);
+            this.setState({
+                user
+            });
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+
+
+
+
     render() {
         return (
 
-
-            <View style={styles.photoContainer}>
-                <RkModalImg source={require('../images/banana1.jpg')}
-                    style={styles.photoItems}
-                    modalImgStyle={styles.modalImg}
-                    modalStyle={styles.modal}
-                    transparent={false} />
-                <RkModalImg source={require('../images/banana2.jpeg')}
-                    style={styles.photoItems}
-                    modalImgStyle={styles.modalImg}
-                    modalStyle={styles.modal} />
-                <RkModalImg source={require('../images/banana3.jpg')}
-                    style={styles.photoItems}
-                    modalImgStyle={styles.modalImg}
-                    modalStyle={styles.modal} />
-                <RkModalImg source={require('../images/banana4.jpg')}
-                    style={styles.photoItems}
-                    modalImgStyle={styles.modalImg}
-                    modalStyle={styles.modal} />
-                <RkModalImg source={require('../images/banana1.jpg')}
-                    style={styles.photoItems}
-                    modalImgStyle={styles.modalImg}
-                    modalStyle={styles.modal} />
-                <RkModalImg source={require('../images/banana2.jpeg')}
-                    style={styles.photoItems}
-                    modalImgStyle={styles.modalImg}
-                    modalStyle={styles.modal} />
-                <RkModalImg source={require('../images/banana3.jpg')}
-                    style={styles.photoItems}
-                    modalImgStyle={styles.modalImg}
-                    modalStyle={styles.modal} />
-                <RkModalImg source={require('../images/banana4.jpg')}
-                    style={styles.photoItems}
-                    modalImgStyle={styles.modalImg}
-                    modalStyle={styles.modal} />
-                <RkModalImg source={require('../images/banana1.jpg')}
-                    style={styles.photoItems}
-                    modalImgStyle={styles.modalImg}
-                    modalStyle={styles.modal} />
-                <RkModalImg source={require('../images/banana2.jpeg')}
-                    style={styles.photoItems}
-                    modalImgStyle={styles.modalImg}
-                    modalStyle={styles.modal} />
-                <RkModalImg source={require('../images/banana3.jpg')}
-                    style={styles.photoItems}
-                    modalImgStyle={styles.modalImg}
-                    modalStyle={styles.modal} />
-                <RkModalImg source={require('../images/banana4.jpg')}
-                    style={styles.photoItems}
-                    modalImgStyle={styles.modalImg}
-                    modalStyle={styles.modal} />
-                <RkModalImg source={require('../images/banana1.jpg')}
-                    style={styles.photoItems}
-                    modalImgStyle={styles.modalImg}
-                    modalStyle={styles.modal} />
-                <RkModalImg source={require('../images/banana2.jpeg')}
-                    style={styles.photoItems}
-                    modalImgStyle={styles.modalImg}
-                    modalStyle={styles.modal} />
-                <RkModalImg source={require('../images/banana3.jpg')}
-                    style={styles.photoItems}
-                    modalImgStyle={styles.modalImg}
-                    modalStyle={styles.modal} />
-                <RkModalImg source={require('../images/banana4.jpg')}
-                    style={styles.photoItems}
-                    modalImgStyle={styles.modalImg}
-                    modalStyle={styles.modal} />
-                <RkModalImg source={require('../images/banana1.jpg')}
-                    style={styles.photoItems}
-                    modalImgStyle={styles.modalImg}
-                    modalStyle={styles.modal} />
-                <RkModalImg source={require('../images/banana2.jpeg')}
-                    style={styles.photoItems}
-                    modalImgStyle={styles.modalImg}
-                    modalStyle={styles.modal} />
-                <RkModalImg source={require('../images/banana3.jpg')}
-                    style={styles.photoItems}
-                    modalImgStyle={styles.modalImg}
-                    modalStyle={styles.modal} />
-                <RkModalImg source={require('../images/banana4.jpg')}
-                    style={styles.photoItems}
-                    modalImgStyle={styles.modalImg}
-                    modalStyle={styles.modal} />
-                <RkModalImg source={require('../images/banana1.jpg')}
-                    style={styles.photoItems}
-                    modalImgStyle={styles.modalImg}
-                    modalStyle={styles.modal} />
-                <RkModalImg source={require('../images/banana2.jpeg')}
-                    style={styles.photoItems}
-                    modalImgStyle={styles.modalImg}
-                    modalStyle={styles.modal} />
-                <RkModalImg source={require('../images/banana3.jpg')}
-                    style={styles.photoItems}
-                    modalImgStyle={styles.modalImg}
-                    modalStyle={styles.modal} />
-                <RkModalImg source={require('../images/banana4.jpg')}
-                    style={styles.photoItems}
-                    modalImgStyle={styles.modalImg}
-                    modalStyle={styles.modal} />
-                <RkModalImg source={require('../images/banana1.jpg')}
-                    style={styles.photoItems}
-                    modalImgStyle={styles.modalImg}
-                    modalStyle={styles.modal} />
-                <RkModalImg source={require('../images/banana2.jpeg')}
-                    style={styles.photoItems}
-                    modalImgStyle={styles.modalImg}
-                    modalStyle={styles.modal} />
-                <RkModalImg source={require('../images/banana3.jpg')}
-                    style={styles.photoItems}
-                    modalImgStyle={styles.modalImg}
-                    modalStyle={styles.modal} />
-                <RkModalImg source={require('../images/banana4.jpg')}
-                    style={styles.photoItems}
-                    modalImgStyle={styles.modalImg}
-                    modalStyle={styles.modal} />
-                <RkModalImg source={require('../images/banana1.jpg')}
-                    style={styles.photoItems}
-                    modalImgStyle={styles.modalImg}
-                    modalStyle={styles.modal} />
-                <RkModalImg source={require('../images/banana2.jpeg')}
-                    style={styles.photoItems}
-                    modalImgStyle={styles.modalImg}
-                    modalStyle={styles.modal} />
-                <RkModalImg source={require('../images/banana3.jpg')}
-                    style={styles.photoItems}
-                    modalImgStyle={styles.modalImg}
-                    modalStyle={styles.modal} />
-                <RkModalImg source={require('../images/banana4.jpg')}
-                    style={styles.photoItems}
-                    modalImgStyle={styles.modalImg}
-                    modalStyle={styles.modal} />
-                <RkModalImg source={require('../images/banana1.jpg')}
-                    style={styles.photoItems}
-                    modalImgStyle={styles.modalImg}
-                    modalStyle={styles.modal} />
-                <RkModalImg source={require('../images/banana2.jpeg')}
-                    style={styles.photoItems}
-                    modalImgStyle={styles.modalImg}
-                    modalStyle={styles.modal} />
-                <RkModalImg source={require('../images/banana3.jpg')}
-                    style={styles.photoItems}
-                    modalImgStyle={styles.modalImg}
-                    modalStyle={styles.modal} />
-                <RkModalImg source={require('../images/banana4.jpg')}
-                    style={styles.photoItems}
-                    modalImgStyle={styles.modalImg}
-                    modalStyle={styles.modal} />
-            </View>
+            <View><Text>Hi</Text></View>
+            // <View style={styles.photoContainer}>
+            //     <RkModalImg source={this.state.user.image}
+            //         style={styles.photoItems}
+            //         modalImgStyle={styles.modalImg}
+            //         modalStyle={styles.modal}
+            //         transparent={false} />
+            // </View>
 
 
         )
