8026e1893a77d2d8ed96a45ea5600d48385a6c81
Josh Kincheloe <jkinchloe33@gmail.com>
2018-03-09 11:17:41 -0600

    In Carousel.js, used the allLocations fetch call to populate the carousel.

--- components/carousel.js
+++ components/carousel.js
@@ -33,27 +33,18 @@ export class MyCarousel extends Component {
 
 
     render() {
+        const dataCarousel = this.state.locations.map((location, id) => {
+            return {
+                id: location.id,
+                title: location.name,
+                imagePath: location.image
+            }
+        });
 
         return (
             <SwipeableParallaxCarousel
                 data={
-                    [
-                        {
-                            id: 339964,
-                            title: "Sponsored Locations",
-                            imagePath: "http://www.nashvillelifestyles.com/_scripts/img_pp_crop.php?w=632&h=475&img=/_uploads/articles/IMG_82.jpg",
-                        },
-                        {
-                            id: 315635,
-                            imagePath: "http://www.visitmusiccity.com/Content/cms/uploads/TwoOldHippies300x300.png",
-                        },
-
-                        {
-                            id: 339403,
-                            title: "The Gulch",
-                            imagePath: "https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/27750846_10156795484380021_4193555394840914156_n.jpg?oh=67ebe617547dadff7f9a358a22fa333d&oe=5B49CA7A",
-                        },
-                    ]
+                    dataCarousel
                 }
                 titleColor={'white'}
                 navigation={'true'}
