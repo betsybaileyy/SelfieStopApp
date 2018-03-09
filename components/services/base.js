dc83c9998af2af4f87cb7aef12a191b803eb7367
SkyMX99 - <alex.lewis@gmail.com>
2018-03-09 16:52:26 -0600

    working profile pic update

--- components/services/base.js
+++ components/services/base.js
@@ -1,6 +1,6 @@
 import { AsyncStorage } from 'react-native';
 const AUTH_TOKEN_KEY = 'authtoken';
-const BASE_URL = 'https://cmgpkgrieh.localtunnel.me';
+const BASE_URL = 'https://ybnxktesau.localtunnel.me';
 
 async function setAuthToken(token) {
     return await AsyncStorage.setItem(AUTH_TOKEN_KEY, `Bearer ${token}`);
