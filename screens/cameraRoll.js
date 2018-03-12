646074580f50a9ca3fc8c947d8f1b7a94cdda9af
SkyMX99 - <alex.lewis@gmail.com>
2018-03-12 09:40:11 -0500

    updates camera roll

--- screens/cameraRoll.js
+++ screens/cameraRoll.js
@@ -12,15 +12,31 @@ import {
 import { withNavigation } from 'react-navigation';
 import CameraRollPicker from 'react-native-camera-roll-picker';
 import * as imageService from '../components/services/images';
+import * as locationService from '../components/services/locations';
+import { RkButton, RkCard, RkTheme, RkText, RkChoice, RkChoiceGroup } from 'react-native-ui-kitten';
 
 
 export default class CameraRoll extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            image: ''
+            locations: [],
+
         };
         imageProps = this.props;
+    }
+
+    async componentDidMount() {
+        try {
+            const locations = await locationService.allLocations()
+            this.setState({ locations }); //this.state.locations
+        } catch (err) {
+            if (err.message) {
+                this.setState({ feedbackMessage: err.message });
+            }
+            console.log(err);
+        }
+        console.log(this.state.locations);
     }
 
     getSelectedImages(images) {
@@ -49,15 +65,40 @@ export default class CameraRoll extends Component {
 
     }
 
+    handleLocationChange(location) {
+        console.log('test change');
+    }
 
     render() {
+
+
+
         return (
-            <ScrollView>
-                <CameraRollPicker
-                    callback={this.getSelectedImages}
-                    maximum={1}
-                />
-            </ScrollView>
+            <View>
+                <ScrollView>
+                    <RkChoiceGroup
+                        selectedIndex={1} radio
+                    >
+                        {this.state.locations.map((location, id) => {
+                            return (
+                                <TouchableOpacity onPress={(location) => this.handleLocationChange(location)} choiceTrigger >
+                                    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                                        <RkChoice rkType='radio' />
+                                        <Text>{location.name}</Text>
+                                    </View>
+                                </TouchableOpacity>
+                            )
+                        })}
+                    </RkChoiceGroup>
+                </ScrollView>
+                <ScrollView>
+                    <CameraRollPicker
+                        callback={this.getSelectedImages}
+                        maximum={1}
+                    />
+                </ScrollView>
+
+            </View>
         )
     }
 }
